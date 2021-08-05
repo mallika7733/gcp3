2900bb8002d6463d0251850a7fc119c985592767
mallika7733 <87751601+mallika7733@users.noreply.github.com>
2021-08-05 12:58:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Mallika 1927197'
 
 
 if __name__ == '__main__':
